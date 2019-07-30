9acbd4462474ac2b89b6fe85c8a4ca0ad9d33cc8
sherylynn <352281674@qq.com>
2019-07-30 22:44:18 +0800

    change java to kotlin

---
+++ app/src/main/java/com/sherylynn/testUtils/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sherylynn.testUtils
+
+import android.app.*
+import android.os.*
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
