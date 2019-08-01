b48eef0a53a48f01eb919871ecffa48abb31a56e
sherylynn <352281674@qq.com>
2019-08-01 23:28:19 +0800

    add button for hook

--- app/src/main/java/com/sherylynn/testUtils/MainActivity.kt
+++ app/src/main/java/com/sherylynn/testUtils/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sherylynn.testUtils
 
 import android.app.*
 import android.os.*
+import android.widget.Toast
+import kotlinx.android.synthetic.main.main.*
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        button.setOnClickListener{
+            Toast.makeText(this@MainActivity,toastMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+    fun toastMessage():String{
+        return "I am not hooked";
     }
 }
