4c414a13f46f3b2c4b5b53f32aeff611aa6faafe
sherylynn <352281674@qq.com>
2019-08-04 13:34:02 +0800

    add wechat hook

--- app/src/main/java/com/sherylynn/testUtils/HookEntry.kt
+++ app/src/main/java/com/sherylynn/testUtils/HookEntry.kt
@@ -1,8 +1,10 @@
 package com.sherylynn.testUtils
 
-import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodReplacement
-import de.robv.android.xposed.XposedHelpers
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import de.robv.android.xposed.*
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 /**
@@ -13,6 +15,7 @@ class HookEntry : IXposedHookLoadPackage {
 
     @Throws(Throwable::class)
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        /*
         if (lpparam.packageName == BuildConfig.APPLICATION_ID) {
             XposedHelpers.findAndHookMethod(
                     MainActivity::class.java.name,
@@ -20,6 +23,53 @@ class HookEntry : IXposedHookLoadPackage {
                     "toastMessage",
                     XC_MethodReplacement.returnConstant("I had Hooked!!!!")
             )
+        }else
+         */
+         if(lpparam?.packageName=="com.tencent.mm"){
+            var hookClass="com.tencent.mm.plugin.wallet.balance.ui.WalletBalanceManagerUI";
+            var hookMethodName="onCreate";
+            XposedHelpers.findAndHookMethod(
+                    hookClass,
+                    lpparam.classLoader,
+                    hookMethodName,
+                    Bundle::class,
+                    object: XC_MethodHook(){
+                        override fun afterHookedMethod(param: MethodHookParam?) {
+                            var walletBalanceActivity=param!!.thisObject;
+                            var allField=walletBalanceActivity.javaClass.declaredFields;
+                            for (field in allField){
+                                field.isAccessible=true;
+                                var fieldObject = field.get(walletBalanceActivity);
+                                if(fieldObject!=null && fieldObject is TextView){
+                                    fieldObject.addTextChangedListener(TextViewWatcher (fieldObject));
+                                    XposedBridge.log(field.getName() + ", " + fieldObject.getText().toString());
+                                }
+                            }
+                        }
+                    }
+            );
         }
+    }
+}
+class TextViewWatcher(private val textView: TextView) : TextWatcher {
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun afterTextChanged(p0: Editable) {
+        var text =p0.toString()
+        XposedBridge.log("textView内容: " + text);
+        if(text.contains("¥")){
+            textView.removeTextChangedListener(this)
+            textView.text=LAST_TEXT
+            textView.addTextChangedListener(this)
+        }
+    }
+    companion object{
+        val LAST_TEXT="20000.00";
     }
 }
---
+++ app/src/main/java/com/sherylynn/testUtils/MainHook.kt
@@ -0,0 +1,72 @@
+package com.sherylynn.testUtils
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import de.robv.android.xposed.*
+
+import java.lang.reflect.Field
+
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class MainHook : IXposedHookLoadPackage {
+    @Throws(Throwable::class)
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        /* 
+        if (lpparam.packageName == BuildConfig.APPLICATION_ID) {
+            XposedHelpers.findAndHookMethod(
+                    MainActivity::class.java.name,
+                    lpparam.classLoader,
+                    "toastMessage",
+                    XC_MethodReplacement.returnConstant("I had Hooked!!!!")
+            )
+        }
+        */
+        if (lpparam.packageName == "com.tencent.mm") {
+            val hookClass = "com.tencent.mm.plugin.wallet.balance.ui.WalletBalanceManagerUI"
+            val hookMethodName = "onCreate"
+
+            XposedHelpers.findAndHookMethod(hookClass, lpparam.classLoader, hookMethodName, Bundle::class.java, object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam?) {
+                    val walletBalanceActivity = param!!.thisObject
+                    val allField = walletBalanceActivity.javaClass.declaredFields
+                    for (field in allField) {
+                        field.isAccessible = true
+                        val fieldObject = field.get(walletBalanceActivity)
+                        if (fieldObject != null && fieldObject is TextView) {
+                            fieldObject.addTextChangedListener(TextViewWatcher2(fieldObject))
+                            XposedBridge.log(field.name + ", " + fieldObject.text.toString())
+                        }
+                    }
+                }
+            })
+        }
+    }
+}
+
+class TextViewWatcher2(private val textView: TextView) : TextWatcher {
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable) {
+        val text = s.toString()
+        XposedBridge.log("textView内容: $text")
+        if (text.contains("¥")) {
+            textView.removeTextChangedListener(this)
+            textView.text = LAST_TEXT
+            textView.addTextChangedListener(this)
+        }
+    }
+
+    companion object {
+        val LAST_TEXT = "200000.00"
+    }
+}
